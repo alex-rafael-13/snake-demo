167ee2232d319e46842f8e444b04ec2eeef1b6b9
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-11-13 23:11:57 -0800

    snake segments complete

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -29,10 +29,34 @@ public class Snake : MonoBehaviour
 
     private void FixedUpdate()
     {
+        for(int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+
+
         this.transform.position = new Vector3(
             Mathf.Round(this.transform.position.x) + _direction.x,
             Mathf.Round(this.transform.position.y) + _direction.y,
             0.0f
         );
+    }
+
+
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }
+
     }
 }
